d8ced7dce97b90ae35029859ed51bb3e9a76fcdc
Reuben Sweet <computergeek14@gmail.com>
2018-02-28 13:26:47 -0600

    Added Player funtionality

--- js/app.js
+++ js/app.js
@@ -23,7 +23,6 @@ var Enemy = function () {
         return 100 + Math.floor(Math.random() * 512);
     }
 
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.speed = speed();
@@ -54,7 +53,20 @@ Enemy.prototype.update = function (dt) {
     // all computers.
     this.x += this.speed * dt;
 
-    
+    // when off canvas, reset position of enemy to move across again
+    if (this.x > 550) {
+        this.x = -100;
+        this.speed = 100 + Math.floor(Math.random() * 512);
+    }
+
+    // Check for collision between player and enemies
+    if (player.x < this.x + 60 &&
+        player.x + 37 > this.x &&
+        player.y < this.y + 25 &&
+        30 + player.y > this.y) {
+        player.x = 200;
+        player.y = 380;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -73,7 +85,24 @@ var Player = function (x, y, speed) {
 }
 
 Player.prototype.update = function (dt) {
-    
+    // Prevent player from moving beyond canvas wall boundaries
+    if (this.y > 380) {
+        this.y = 380;
+    }
+
+    if (this.x > 400) {
+        this.x = 400;
+    }
+
+    if (this.x < 0) {
+        this.x = 0;
+    }
+
+    // Check for player reaching top of canvas and winning the game
+    if (this.y < 0) {
+        this.x = 200;
+        this.y = 380;
+    }
 }
 
 Player.prototype.render = function () {
@@ -81,7 +110,20 @@ Player.prototype.render = function () {
 }
 
 Player.prototype.handleInput = function (keyPress) {
-    
+    switch (keyPress) {
+        case 'left':
+            this.x -= this.speed + 50;
+            break;
+        case 'up':
+            this.y -= this.speed + 30;
+            break;
+        case 'right':
+            this.x += this.speed + 50;
+            break;
+        case 'down':
+            this.y += this.speed + 30;
+            break;
+    }
 }
 
 // Now instantiate your objects.
@@ -94,8 +136,6 @@ var player = new Player(200, 380, 50);
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 allEnemies = [firstEnemy, secondEnemy, thirdEnemy];
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
